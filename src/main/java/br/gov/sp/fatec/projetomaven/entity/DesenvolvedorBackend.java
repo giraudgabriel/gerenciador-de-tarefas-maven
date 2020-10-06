37d94f257d99c4a2e53fe13962450456a5865b8d
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2020-10-06 22:25:34 +0000

    Single Table Devs

--- src/main/java/br/gov/sp/fatec/projetomaven/entity/DesenvolvedorBackend.java
+++ src/main/java/br/gov/sp/fatec/projetomaven/entity/DesenvolvedorBackend.java
@@ -1,11 +1,8 @@
 package br.gov.sp.fatec.projetomaven.entity;
 
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
 import javax.persistence.DiscriminatorValue;
 
 @DiscriminatorValue("B")
-@DiscriminatorColumn(name = "cargo", discriminatorType = DiscriminatorType.STRING)
 
 public class DesenvolvedorBackend extends Desenvolvedor{
     
--- src/main/java/br/gov/sp/fatec/projetomaven/entity/DesenvolvedorFrontend.java
+++ src/main/java/br/gov/sp/fatec/projetomaven/entity/DesenvolvedorFrontend.java
@@ -1,11 +1,8 @@
 package br.gov.sp.fatec.projetomaven.entity;
 
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
 import javax.persistence.DiscriminatorValue;
 
 @DiscriminatorValue("F")
-@DiscriminatorColumn(name = "cargo", discriminatorType = DiscriminatorType.STRING)
 public class DesenvolvedorFrontend extends Desenvolvedor {
 
 }
