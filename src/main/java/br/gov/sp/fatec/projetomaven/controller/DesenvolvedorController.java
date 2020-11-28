a29f0f8215aa220d2d7b537cc9a1f7f8cb063c85
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2020-11-28 02:40:38 +0000

    Put dev

--- src/main/java/br/gov/sp/fatec/projetomaven/controller/DesenvolvedorController.java
+++ src/main/java/br/gov/sp/fatec/projetomaven/controller/DesenvolvedorController.java
@@ -15,7 +15,7 @@ import br.gov.sp.fatec.projetomaven.dao.DesenvolvedorDaoJpa;
 import br.gov.sp.fatec.projetomaven.entity.Desenvolvedor;
 
 public class DesenvolvedorController extends HttpServlet {
-     
+
     private static final long serialVersionUID = -6692052149075365360L;
 
     @Override
@@ -40,7 +40,7 @@ public class DesenvolvedorController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-         ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
         Desenvolvedor desenvolvedor = mapper.readValue(req.getReader(), Desenvolvedor.class);
 
         DesenvolvedorDao desenvolvedorDao = new DesenvolvedorDaoJpa();
@@ -53,9 +53,47 @@ public class DesenvolvedorController extends HttpServlet {
 
         resp.setStatus(201);
 
-        String location = req.getServerName() + ":" + req.getServerPort() + req.getContextPath() + "/dev?id=" + desenvolvedor.getId();
+        String location = req.getServerName() + ":" + req.getServerPort() + req.getContextPath() + "/dev?id="
+                + desenvolvedor.getId();
 
         resp.setHeader("Location", location);
+
+        PrintWriter out = resp.getWriter();
+
+        out.print(desenvolvedorJson);
+        out.flush();
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.doDelete(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Long id = Long.valueOf(req.getParameter("id"));
+        
+        ObjectMapper mapper = new ObjectMapper();
+        Desenvolvedor desenvolvedor = mapper.readValue(req.getReader(), Desenvolvedor.class);
+
+        DesenvolvedorDao desenvolvedorDao = new DesenvolvedorDaoJpa();
+        Desenvolvedor dev = desenvolvedorDao.buscarPorId(id);
+
+        dev.setNome(desenvolvedor.getNome());
+        dev.setNomeUsuario(desenvolvedor.getNomeUsuario());
+        dev.setFuncoes(desenvolvedor.getFuncoes());
+        dev.setSenha(desenvolvedor.getSenha());
+        dev.setTarefas(desenvolvedor.getTarefas());
+
+        desenvolvedorDao.salvar(dev);
+
+        String desenvolvedorJson = mapper.writeValueAsString(dev);
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        resp.setStatus(200);
 
         PrintWriter out = resp.getWriter();
 
