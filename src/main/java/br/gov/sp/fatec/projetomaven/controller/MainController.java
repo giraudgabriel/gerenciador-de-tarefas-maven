7c06631f2ff48524a459cdac0f1c17258f599e4b
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2020-11-28 00:25:48 +0000

    Inicializando um servlet

---
+++ src/main/java/br/gov/sp/fatec/projetomaven/controller/MainController.java
@@ -0,0 +1,19 @@
+package br.gov.sp.fatec.projetomaven.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainController extends HttpServlet {
+
+    private static final long serialVersionUID = -7802229025454480671L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("Bem vindo!");
+    }
+
+}
